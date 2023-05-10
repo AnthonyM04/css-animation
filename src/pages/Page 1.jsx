2c3cc7d8bcb70f861f4bd1d8e19c96029640f021
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-05-10 15:28:29 -0400

    animationz

--- src/pages/Page 1.jsx
+++ src/pages/Page 1.jsx
@@ -1,7 +1,67 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 export default function Page1() {
     return(
-        <>
-            <h2>Page 1</h2>
-        </>
+        <Container>
+            <Row>
+                <Col>H2: Element</Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <div className="box scale">
+                        <p className="text-center text-white">Box-Scale</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="box scale-x">
+                        <p className="text-center text-white">Scale-x</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="box rotate">
+                        <p className="text-center text-white">rotate</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="box skew">
+                        <p className="text-center text-white">Skew</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="box skew2">
+                        <p className="text-center text-white">Skew2</p>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="box translate">
+                        <p className="text-center text-white">Translate</p>
+                    </div>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                <div className="box1 bgcolor1">
+                        <p className="text-center text-white">bgcolor 1</p>
+                    </div>
+                </Col>
+                <Col>
+                <div className="box2 bgcolor2">
+                        <p className="text-center text-white">bgcolor 2</p>
+                    </div>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                <div className="boxrace race1">Linear</div>
+                <div className="boxrace race2">Ease</div>
+                <div className="boxrace race3">Ease-in</div>
+                <div className="boxrace race4">Ease-out</div>
+                <div className="boxrace race5">Ease-in-out</div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
